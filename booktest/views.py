44e65164cc616de5a43b48f639d5ca32612ace18
pengpeng12 <18236933571@139.com>
2018-01-30 19:06:56 +0800

    change pk=2

--- booktest/views.py
+++ booktest/views.py
@@ -1,8 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import *
 
 # Create your views here.
 def index(request):
-    hero = HeroInfo.objects.get(pk=1)
+    hero = HeroInfo.objects.get(pk=2)
     context = {'hero':hero}
     return render(request, 'booktest/index.html',context)
