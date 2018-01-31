d710e1a7ec54fc5e3d4281556fbf84af7a0c200b
pengpeng12 <18236933571@139.com>
2018-01-31 18:54:21 +0800

    模板block

--- booktest/urls.py
+++ booktest/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from booktest import views
 
 urlpatterns=[
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^([a-zA-Z]+)/(\d+)$',views.show, name='show'),
+    url(r'^index2$', views.index2, name='index2'),
+    url(r'^user1', views.user1, name='user1'),
+    url(r'^user2', views.user2, name='user2'),
 ]
--- booktest/views.py
+++ booktest/views.py
@@ -1,9 +1,28 @@
+#coding=utf-8
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import *
 
 # Create your views here.
 def index(request):
-    hero = HeroInfo.objects.get(pk=2)
-    context = {'hero':hero}
+    # hero = HeroInfo.objects.get(pk=2)
+    # context = {'hero':hero}
+
+    # list = HeroInfo.objects.filter(hname='ff')
+    list = HeroInfo.objects.all()
+    context = {'list':list}
     return render(request, 'booktest/index.html',context)
+
+def show(request,text, id):
+    context = {'text':text,'id':id}
+    return render(request, 'booktest/show.html', context)
+
+#用于练习模板的继承
+def index2(request):
+    return render(request, 'booktest/indexExtendBase.html')
+
+def user1(request):
+    return render(request, 'booktest/user1.html')
+
+def user2(request):
+    return render(request, 'booktest/user2.html')
